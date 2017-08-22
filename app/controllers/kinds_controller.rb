d6d1f6db710c00d3c3f829e52058aabdbd4afdce
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-08-22 01:27:32 +0000

    Adicionando autenticação http basic e digest

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,5 +1,12 @@
 class KindsController < ApplicationController
   before_action :set_kind, only: [:show, :update, :destroy]
+  
+  #include ActionController::HttpAuthentication::Basic::ControllerMethods
+  #http_basic_authenticate_with name: "marcus", password: "secret"
+
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = {"marcus" => Digest::MD5.hexdigest(["marcus","Application","secret"].join(":"))}
+  before_action :authenticate
 
   # GET /kinds
   def index
@@ -52,5 +59,11 @@ class KindsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
     end
 end
