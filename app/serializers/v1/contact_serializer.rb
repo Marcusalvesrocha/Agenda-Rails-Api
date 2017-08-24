658a6e2ed5341482af9f0bafa80cf841caa35223
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-08-24 19:10:21 +0000

    Controlando versao com segmento de url

--- app/serializers/v1/contact_serializer.rb
+++ app/serializers/v1/contact_serializer.rb
@@ -1,19 +1,20 @@
-class ContactSerializer < ActiveModel::Serializer
+module V1
+	class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate#, :author
 
   belongs_to :kind do
-    link(:related) { contact_kind_url(object.id)}
+    link(:related) { v1_contact_kind_url(object.id)}
   end
   
   has_many :phones do
-    link(:related) { contact_phones_url(object.id)}
+    link(:related) { v1_contact_phones_url(object.id)}
   end
 
   has_one :address do
-    link(:related) { contact_address_url(object.id)}
+    link(:related) { v1_contact_address_url(object.id)}
   end
 
-  link(:self) { contact_url(object.id)}
+  link(:self) { v1_contact_url(object.id)}
   
 
   #def author
@@ -30,4 +31,6 @@ class ContactSerializer < ActiveModel::Serializer
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
+end
+
 end
