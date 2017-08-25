bbe8cfa326b8b8f2ed009805eb7a789ba5645f41
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-08-25 19:47:11 +0000

    Criação do ErrorSerializer para padronização da resposta de erro

--- app/serializers/v1/contact_serializer.rb
+++ app/serializers/v1/contact_serializer.rb
@@ -1,36 +1,37 @@
 module V1
 	class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate#, :author
 
-  belongs_to :kind do
-    link(:related) { v1_contact_kind_url(object.id)}
+    attributes :id, :name, :email, :birthdate#, :author
+
+    belongs_to :kind do
+      link(:related) { v1_contact_kind_url(object.id)}
+    end
+    
+    has_many :phones do
+      link(:related) { v1_contact_phones_url(object.id)}
+    end
+
+    has_one :address do
+      link(:related) { v1_contact_address_url(object.id)}
+    end
+
+    link(:self) { v1_contact_url(object.id)}
+    
+
+    #def author
+    #  "Marcus"
+    #end
+
+    meta do
+      {author: "Marcus Rocha"}
+    end
+
+    def attributes(*args)
+      h = super(*args)
+      #h[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
+      h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+      h
+    end
   end
-  
-  has_many :phones do
-    link(:related) { v1_contact_phones_url(object.id)}
-  end
-
-  has_one :address do
-    link(:related) { v1_contact_address_url(object.id)}
-  end
-
-  link(:self) { v1_contact_url(object.id)}
-  
-
-  #def author
-  #  "Marcus"
-  #end
-
-  meta do
-    {author: "Marcus Rocha"}
-  end
-
-  def attributes(*args)
-    h = super(*args)
-    #h[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
-    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
-    h
-  end
-end
 
 end
